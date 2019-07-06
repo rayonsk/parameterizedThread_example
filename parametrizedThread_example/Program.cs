eab84df5e26ee99b3e21b201edb6dc156d97411e
rayon susan koshy <rayonsusan@gmail.com>
2019-07-06 12:34:28 -0400

    imported the project from local

---
+++ parametrizedThread_example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace threadTest_2
+{
+    class Program
+    {
+        public static void childThread(object obj)
+        {
+            for (int i = 0; i < (int)obj; i++)
+            {
+                Console.WriteLine("the thread = {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread tobj = new Thread(new ParameterizedThreadStart(childThread));
+            tobj.Start(5);
+
+            //Console.ReadLine();
+
+        }
+    }
+}
